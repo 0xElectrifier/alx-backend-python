a89db34db4a1fb07a473d08e73e0bf6ee2902d09
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-02 22:53:14 +0100

    Resolved checker error, incorrect output

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple which contains a string and/or float"""
-    return (k, v);
+    return (k, v ** 2)
