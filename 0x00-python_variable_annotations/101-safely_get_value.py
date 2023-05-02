7da92c77f48fb745375b875ce8656e5c53f6cef4
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-02 23:43:11 +0100

    Resolving checker error, incomplete annotation of parameters

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,8 @@ T = TypeVar('T')
 def safely_get_value(dct: Mapping,
                      key: Any,
                      default: Union[T, None] = None
-                     ) -> Union[Any, ]:
+                     ) -> Union[Any, T]:
+    '''Retrieves a value from a dict using a given key.'''
     if key in dct:
         return dct[key]
     else:
