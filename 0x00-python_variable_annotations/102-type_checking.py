fe136d0ffceaae55311b371977c30fb3588d170f
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-03 00:03:17 +0100

    Resolving checker error

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """Defines a type-annotated function for Task 12."""
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
 
-def zoom_array(lst: List, factor: float = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Creates multiple copies of items in a tuple, @lst."""
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
-        for i in range(int(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array: List[int] = [12, 72, 91]
+array: Tuple = (12, 72, 91)
 
-zoom_2x: List[int] = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-zoom_3x: List[int] = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
