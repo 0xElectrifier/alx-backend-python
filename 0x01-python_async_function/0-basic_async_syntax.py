b0beaee31bb3fba5f4000608f9197e02438795b0
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-03 12:41:07 +0100

    Changed the lower boundary of range to generate random number from, from float to int

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,10 +3,15 @@
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Async function that waits for random delay"""
+    """Async function that waits for random delay
+    Args:
+        max_delay (int): The maximum delay to return
+    Returns:
+        A random float between 0 and max_delay
+    """
     import asyncio
     import random
-    i = random.uniform(0.0, max_delay)
+    i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
 
     return i
