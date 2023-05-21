cd4464eb22103508e1084e06a1af319f61011f3b
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-21 13:44:45 +0100

    Using assertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,19 +4,32 @@ from parameterized import parameterized
 from typing import ( Dict, Tuple )
 from unittest import TestCase
 
+access_nested_map = __import__("utils").access_nested_map
+
 
 class TestAccessNestedMap(TestCase):
     """
     Subclass of unittest.TestCase for testing the @utils.access_nested_map
-    function
+    function.
     """
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a"), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map: Dict, path: Tuple, expected: int):
-        """Tests the function"""
-        access_nested_map = __import__("utils").access_nested_map
+        """
+        Checks that @utils.access_nested_map function returns expected result.
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Tests @utils.access_nested_map function raises a KeyError when a
+        key isn't found.
+        """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
