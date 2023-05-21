d5fb49113c6fa9929d1dfe20f9d2f326854e20e8
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-05-21 12:54:28 +0100

    Added type-annotation to file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Parameterize a unit test"""
-from unittest import TestCase
 from parameterized import parameterized
+from typing import ( Dict, Tuple )
+from unittest import TestCase
 
 
 class TestAccessNestedMap(TestCase):
@@ -15,7 +16,7 @@ class TestAccessNestedMap(TestCase):
         ({"a": {"b": 2}}, ("a"), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple, expected: int):
         """Tests the function"""
         access_nested_map = __import__("utils").access_nested_map
         self.assertEqual(access_nested_map(nested_map, path), expected)
